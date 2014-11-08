e5127f4c42595d7ab05d4f6557c8c3e3e6a4f1d0
Matthew Thompson <matt@mattathompson.org>
2014-11-08 14:13:40 -0500

    Created some validations for preferences for uniqueness. Created preferences controller with basic actions.

---
+++ app/controllers/preferences_controller.rb
@@ -0,0 +1,44 @@
+class PreferencesController < ApplicationController
+  before_action :find_prefered_clients, except: [:show]
+
+  def show
+    @prefered_clients = current_user.prefered_clients
+  end
+
+
+  def new
+    @clients = Client.all
+
+  end
+
+
+  def create
+    @client = Client.find params[:id]
+    @preference = current_user.prefered_clients
+
+    if @preference << @client
+      redirect_to preferences_path
+    else
+      redirect_to :back
+    end
+
+  end
+
+
+  def destroy
+    @client = current_user.prefered_clients.find params[:client_id]
+
+    if @client.destroy!
+      redirect_to preferences_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def find_prefered_clients
+    @prefered_clients = Client.all
+  end
+
+end
