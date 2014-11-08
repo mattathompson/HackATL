b4c9bcb07c403c4396b69e6aa3dae59bcf5d993c
Matthew Thompson <matt@mattathompson.org>
2014-11-08 10:48:23 -0500

    Create Clients and basic Authentication for them

---
+++ db/migrate/20141108153937_create_clients.rb
@@ -0,0 +1,26 @@
+class CreateClients < ActiveRecord::Migration
+  def change
+    create_table(:clients) do |t|
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      t.datetime :remember_created_at
+
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+
+
+      t.timestamps
+    end
+
+    add_index :clients, :email,                unique: true
+    add_index :clients, :reset_password_token, unique: true
+  end
+end
