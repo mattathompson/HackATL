da1d083b3ff88b1aa491f04cb0cd1b79d48278ec
Matthew Thompson <matt@mattathompson.org>
2014-11-08 18:00:37 -0500

    Creating a Events Controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,30 +1,51 @@
 class EventsController < ApplicationController
 
   def index
-    @events = current_user.events.all
+    @events = current_client.events.all
   end
 
   def show
-
+    @event = current_client.events.find params[:id]
   end
 
   def new
-
+    @event = current_client.events.new
   end
 
   def edit
-
+    @event = current_client.events.find params[:id]
   end
 
   def update
+    @event = current_client.events.find params[:id]
+    @event.update_attributes create_params
 
+    if @event.save!
+      redirect_to @event
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
+    @event = current_client.events.find params[:id]
+
+    if @event.destroy!
+      redirect_to events_path
+    else
+      redirect_to :back
+    end
 
   end
 
   def create
+    @event = current_client.events.new create_params
+
+    if @event.save!
+      redirect_to @event
+    else
+      redirect_to :back
+    end
 
   end
 
@@ -33,7 +54,7 @@ class EventsController < ApplicationController
 
 
   def create_params
-
+    params[:event].permit(:name, :details)
   end
 
 
