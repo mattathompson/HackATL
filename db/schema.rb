5d6dd5b37fe6a70fa7b1f0c8457fa4fc39e5e370
Andy Perez <agperez@gmail.com>
2014-11-09 03:27:36 -0500

    Responsive User Landing / Pref

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -33,13 +32,13 @@ ActiveRecord::Schema.define(version: 20141109064320) do
     t.string   "state"
     t.integer  "zip"
     t.string   "phone"
+    t.string   "color1"
+    t.string   "color2"
+    t.string   "color3"
     t.string   "logo_file_name"
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
-    t.string   "color1"
-    t.string   "color2"
-    t.string   "color3"
   end
 
   add_index "clients", ["email"], name: "index_clients_on_email", unique: true
